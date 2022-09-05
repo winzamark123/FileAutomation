1eb6080c3ce59f51c6295c4c260bb720f3599b30
Win Cheng <wincheng@Wins-MacBook-Pro.local>
2022-09-05 20:46:32 +0700

    Used py2app to create an application on Desktop for use IRL

--- DownloadsAutomation.py
+++ DownloadsAutomation.py
@@ -1,9 +1,5 @@
 import os
 import shutil
-import time 
-from watchdog.observers import Observer
-from watchdog.events import FileSystemEventHandler
-
 
 #moving the files using shutil
 def Move_Files(source_dir, file_name, dest_path):
@@ -21,79 +17,60 @@ def Move_Files(source_dir, file_name, dest_path):
     shutil.move(src_path, dest_path)
     
 
-class Scan_Files(FileSystemEventHandler):
+class Scan_Files:
+    #Starting Directory
+    source_dir = "/Users/wincheng/Downloads"
+
+    #Destination Directory
+    CSV_dir = "/Users/wincheng/Downloads/CSV_Downloads/"
+    Vids_dir = "/Users/wincheng/Downloads/Vids_Downloads/"
+    Photos_dir = "/Users/wincheng/Downloads/Photos_Downloads/"
+    PDF_dir = "/Users/wincheng/Downloads/PDF_Downloads/"
+
+    #=======================================================================
+    #STARTING THE PROGRAM
+    #=======================================================================
+
+    # Scan the directory and get an iterator of os.DirEntry objects
+    # corresponding to entries in it using os.scandir() method
+    obj = os.scandir(source_dir)
     
-    #function works when the directory file is modified
-    def on_modified(self, event):
+    # List all files and directories in the specified source_dir
+    with os.scandir(source_dir) as itr:
 
-        #Starting Directory
-        source_dir = "/Users/wincheng/Downloads"
+    #starting the loop running through all files contained within the directory 
+        for entry in itr :
+            if entry.is_dir() or entry.is_file():
 
-        #Destination Directory
-        CSV_dir = "/Users/wincheng/Downloads/CSV_Downloads/"
-        Vids_dir = "/Users/wincheng/Downloads/Vids_Downloads/"
-        Photos_dir = "/Users/wincheng/Downloads/Photos_Downloads/"
-        PDF_dir = "/Users/wincheng/Downloads/PDF_Downloads/"
+                #init destination as Downloads folder in case of other type of files
+                file_name = entry.name
+                dest_path = source_dir
 
-        #=======================================================================
-        #STARTING THE PROGRAM
-        #=======================================================================
+                #ignore the current folders within the download directory
+                if file_name.lower() in ["pdf_downloads", "csv_downloads", \
+                    "photos_downloads", "vids_downloads"] or file_name.startswith("."):
+                    continue
+                
+                #specifying the destination folder based on types of file
+                if file_name.endswith(".png") or file_name.endswith(".jpeg"):
+                    dest_path = Photos_dir
 
-        # Scan the directory and get an iterator of os.DirEntry objects
-        # corresponding to entries in it using os.scandir() method
-        obj = os.scandir(source_dir)
+                elif file_name.endswith(".csv"):
+                    dest_path = CSV_dir
+
+                elif file_name.endswith(".pdf"):
+                    dest_path = PDF_dir
+
+                elif file_name.endswith(".mp4"):
+                    dest_path = Vids_dir
+                
+                print(file_name)
+                Move_Files(source_dir, file_name, dest_path)
         
-        # List all files and directories in the specified source_dir
-        with os.scandir(source_dir) as itr:
-
-        #starting the loop running through all files contained within the directory 
-            for entry in itr :
-                if entry.is_dir() or entry.is_file():
-
-                    #init destination as Downloads folder in case of other type of files
-                    file_name = entry.name
-                    dest_path = source_dir
-
-                    #ignore the current folders within the download directory
-                    if file_name.lower() in ["pdf_downloads", "csv_downloads", \
-                        "photos_downloads", "vids_downloads"] or file_name.startswith("."):
-                        continue
-                    
-                    #specifying the destination folder based on types of file
-                    if file_name.endswith(".png") or file_name.endswith(".jpeg"):
-                        dest_path = Photos_dir
-
-                    elif file_name.endswith(".csv"):
-                        dest_path = CSV_dir
-
-                    elif file_name.endswith(".pdf"):
-                        dest_path = PDF_dir
-
-                    elif file_name.endswith(".mp4"):
-                        dest_path = Vids_dir
-                    
-                    print(file_name)
-                    Move_Files(source_dir, file_name, dest_path)
-            
-            #closing the scan 
-            obj.close()
-            return 
-
-
-if __name__ == "__main__":
-    event_handler = Scan_Files()
-    observer = Observer()
-    observer.schedule(event_handler, path='/data/', recursive=False)
-    observer.start()
-
-    try:
-        while True:
-            time.sleep(1)
-    except KeyboardInterrupt:
-        observer.stop()
-    observer.join()
+        #closing the scan 
+        obj.close() 
 
 
 
-        
-   
+
+
